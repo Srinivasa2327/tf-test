40690a9f5f00cf2c09bd94b3d127db6182b35f71
Srinivasa2327 <45688676+Srinivasa2327@users.noreply.github.com>
2023-06-05 11:05:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "azurerm" {
   subscription_id = "5959d188-3a85-4a68-98e0-af236b3c2cac"
   alias           = "identity"
   tenant_id       = "a738da5b-465c-4cbd-a7de-1b64c889d37a"
+  use_msi = true
   features {}
 }
 
